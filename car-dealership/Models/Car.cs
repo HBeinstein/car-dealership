9916e5aa87cfa6cce286333fb16a3a1bf8ffb866
Sean Downs <spdowns@protonmail.com>
2020-07-13 13:26:23 -0700

    set Car class to private
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>

--- car-dealership/Models/Car.cs
+++ car-dealership/Models/Car.cs
@@ -3,9 +3,9 @@ namespace Dealership.Models
   public class Car
   {
       // Fields
-    public string MakeModel;
-    public int Price;
-    public int Miles;
+    private string MakeModel;
+    private int Price;
+    private int Miles;
 
       // Constructor
     public Car(string makeModel, int price, int miles) // <---- the final field
